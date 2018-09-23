e17df9b155b121a190cb53ee05a438fd76ee51fa
Infra <vjviana@thoughtworks.com>
2018-09-22 22:59:03 -0300

    Remove function dateNow of Utils

--- src/main/java/br/com/spacebox/common/utils/Utils.java
+++ src/main/java/br/com/spacebox/common/utils/Utils.java
@@ -1,11 +1,5 @@
 package br.com.spacebox.common.utils;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.Date;
-
 public class Utils {
-    public static Date getDateNow() {
-        return Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
-    }
+
 }
